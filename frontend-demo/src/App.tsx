415d67b9ecc66257435148d98bb9c108ed6bacc6
somanth-more <somnath.more@zemosolabs.com>
2024-03-15 17:20:52 +0530

    feat : Create Docker Image

---
+++ frontend-demo/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+   
+        <h1>This is Frontend Docker Application : Create Docker image for this Application</h1> 
+    
+      </header>
+    </div>
+  );
+}
+
+export default App;
