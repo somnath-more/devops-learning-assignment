5689aa554c4844a0009af2c502f4efd0568a3a7b
somanth-more <somnath.more@zemosolabs.com>
2024-03-26 21:42:18 +0530

    feat : CI workflow for frontend application

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,9 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1>This is react Application</h1>
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-       
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
